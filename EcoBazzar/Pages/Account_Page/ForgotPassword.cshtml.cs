4b131425db06419b0f1da313819405fa127a0465
Ahmad Hteit <hoteitahmad32@gmail.com>
2024-06-28 21:30:23 +0300

    Updated Forgot password Page and Model

--- EcoBazzar/Pages/Account_Page/ForgotPassword.cshtml.cs
+++ EcoBazzar/Pages/Account_Page/ForgotPassword.cshtml.cs
@@ -1,3 +1,6 @@
+using EcoBazzar.BindingModel.User;
+using EcoBazzar.Services.UserServices;
+using EcoBazzar.ViewModel.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,24 @@ namespace EcoBazzar.Pages.Account_Page
 {
     public class ForgotPasswordModel : PageModel
     {
+        private readonly IUserServices userServices;
+        public ForgotPasswordModel(IUserServices userServices)
+        {
+            this.userServices = userServices;
+        }
+        [BindProperty]
+        public FPBindingModel binding {  get; set; }
+        public FPViewModel email {  get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("../Index");
         }
     }
 }
