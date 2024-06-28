c899831694c70d7f5a807a3a316a68005d948efa
Mohammad Hamdar <MohammadJHamdar@hotmail.com>
2024-06-28 17:42:31 +0300

    Adding Product SubCategory classes

---
+++ EcoBazzar/DataModel/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace EcoBazzar.DataModel
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image {  get; set; }
+        public string Brand { get; set; }
+        public double Rating { get; set; }
+        public double Weight { get; set; }
+        public int Stock { get; set; }
+        public double Discount { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateModified { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public int SubCategoryId { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
