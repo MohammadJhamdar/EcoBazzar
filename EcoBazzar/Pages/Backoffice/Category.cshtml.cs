85efec92d43c821ed4c621f8e183ce69a44554f6
Mohammad Hamdar <MohammadJHamdar@hotmail.com>
2024-06-30 15:08:47 +0300

    Adding Razor Pages

---
+++ EcoBazzar/Pages/Backoffice/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoBazzar.Pages.Backoffice
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EcoBazzar/Pages/Backoffice/SubCategory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoBazzar.Pages.Backoffice
+{
+    public class SubCategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
